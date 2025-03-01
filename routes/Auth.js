5f7b9a7721004ef6bf186082eda62cf81eda6101
Shruti Kamboj <kamboj.shruti01@gmail.com>
2025-03-01 09:08:18 +0530

    Revert "user and category apis added"
    This reverts commit e60c51e0cb91eb094c7c077691d9338b40fbfa3a.

--- routes/Auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const { userRegister, adminLogin, userLogin } = require("../controllers/auth")
-const router = express.Router()
-
-router.post("/signup", userRegister)
-router.post("/:role/login", adminLogin)
-router.post("/user/login", userLogin)
